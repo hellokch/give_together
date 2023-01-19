c7bd5135e82429e6170f4637b677e918703f91c8
hellokch <hello.kch@gmail.com>
2023-01-19 17:31:38 +0900

    test1

--- give_together/src/main/java/dao/setting.sql
+++ give_together/src/main/java/dao/setting.sql
@@ -25,21 +25,3 @@ intro varchar(50),
 picture varchar(100)
 )
 
-
-num int primary key,
-name varchar(30),
-pass varchar(20),
-subject  varchar(100),
-content  varchar(4000),
-file1 varchar(100),
-regdate date,
-readcnt number(10),
-ref int,
-reflevel number(3),
-refstep number(5),
-boardid varchar(1)
-);
-
-
-
-create sequence boardseq;
