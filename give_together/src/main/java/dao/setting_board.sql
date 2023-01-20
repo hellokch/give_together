974d55bcc1e39d30f1b0a6794ec396981624f4dc
JiHyun-Son <wlgusdl5736@naver.com>
2023-01-20 15:50:15 +0900

    board model and sql
    sql name is "giveboard" to avoid crash. model name is same with our lecture.

---
+++ give_together/src/main/java/dao/setting_board.sql
@@ -0,0 +1,25 @@
+create table giveboard(
+index int primary key,
+id varchar(20),
+not_date date,
+mod_date date,
+title varchar(30),
+context varchar(4000),
+p_type varchar(1),
+end_date date,
+picture varchar(200),
+goal number,
+raised number,
+reword varchar(50),
+location varchar(100),
+act_time varchar(11),
+day varchar(20),
+v_start date,
+v_end date,
+v_type varchar(1),
+field varchar(2),
+boardid varchar(1),
+);
+
+
+create sequence giveboardseq;
---
+++ give_together/src/main/java/model/Board.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.Date;
+
+public class Board {
+	int index;
+	String id;
+	Date not_date;
+	Date mod_date;
+	String title;
+	String context;
+	String p_type;
+	Date end_date;
+	String picture;
+	int goal;
+	int raised;
+	String reword;
+	String location;
+	String act_time;
+	String day;
+	Date v_start;
+	Date v_end;
+	String v_type;
+	String field;
+	String boardid;
+}
