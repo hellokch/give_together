9190d3e757d443dc44420230fcc4cb1e15d4d7af
shannon012 <kyt9694@naver.com>
2023-01-25 18:01:07 +0900

    re2

--- give_together/src/main/java/controller/GiveTogetherController.java
+++ give_together/src/main/java/controller/GiveTogetherController.java
@@ -27,10 +27,10 @@ public class GiveTogetherController extends MskimRequestMapping{
 	@RequestMapping("donation")
 	public String donation(HttpServletRequest request, HttpServletResponse response)  throws Exception {
 System.out.println("To donationPage");
-		
-//		List<Board> list=userdao.giveBoard();
-//		System.out.println("@@@"+list);
-		
+		/*
+		List<Board> list=userdao.giveBoard();
+		System.out.println("@@@"+list);
+		*/
 		return "/view/donationPage.jsp";
 	}
 	
